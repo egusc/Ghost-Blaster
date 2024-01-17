e13eac4a56bc80bd29aa7acc3a530dfc958979cb
Ewan Guscott <ewanguscott@hotmail.co.uk>
2024-01-17 21:37:26 +0000

    Added chase radius and stopping distance for enemy

--- Assets/Enemy/EnemyAI.cs
+++ Assets/Enemy/EnemyAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,17 +7,63 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
-    // Start is called before the first frame update
     void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        distanceToTarget = Vector3.Distance(target.position, this.transform.position);
+        
+
+        if(distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+
+        if(isProvoked)
+        {
+            EngageTarget();
+        }
+
+
+        
+    }
+
+    private void EngageTarget()
+    {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (distanceToTarget < navMeshAgent.stoppingDistance)
+        {
+            Attack();
+        }
+        
+    }
+
+    private void ChaseTarget()
+    {
         navMeshAgent.SetDestination(target.position);
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attacking player");
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = new Color(1, 1, 0, 0.75F);
+        Gizmos.DrawSphere(transform.position, chaseRange);
     }
 }
